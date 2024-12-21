e721b1e5bd3e46c7de250cd564689f9c1b492b66
astisulitio <asti02suliastio@gmail.com>
2024-12-21 21:07:00 +0700

    aa

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+    // Explicitly match files inside the 'tests' folder
     testMatch: [
-      '**/tests/**/*.test.js',  // Look specifically for test files inside the tests folder
-      '**/*.test.js'  // Also match any .test.js file in the root or subdirectories
+      '<rootDir>/tests/**/*.test.js', // Look for any .test.js files inside the 'tests' folder
     ],
   };
   
