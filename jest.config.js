8e95bb230ddcf0f52370e1bc27ce9e2b8a468ed2
astisulitio <asti02suliastio@gmail.com>
2024-12-21 21:18:33 +0700

    aa

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    testEnvironment: 'node',
-    roots: ['<rootDir>/tests'],  // Make sure Jest looks in the 'tests' folder
-    testMatch: [
-      '**/*.test.js',  // Look for files ending in .test.js
-    ],
-    transform: {
-      '^.+\\.js$': 'babel-jest',  // Ensure Babel is used to transform JS files
-    },
-    verbose: true,
+    testMatch: ['**/tests/**/*.test.js', '**/*.test.js'],
   };
   
